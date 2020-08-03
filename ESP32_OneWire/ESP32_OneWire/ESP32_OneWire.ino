7962443210197909b9bdbe0be342ed1a489221c8
Robert Nelson <robertcnelson@gmail.com>
2020-08-03 16:33:16 -0500

    Add Quick Example

---
+++ ESP32_OneWire/ESP32_OneWire/ESP32_OneWire.ino
@@ -0,0 +1,112 @@
+#include <ArduinoJson.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+const char* ssid = "XA10-WiFi-X1";
+const char* password = "x";
+
+//Your Domain name with URL path or IP address with path
+const char* serverName = "http://192.168.3.104:8100/v1/data/for/Pool";
+
+unsigned long lastTime = 0;
+// Set timer to 15 seconds (15000)
+unsigned long timerDelay = 15000;
+
+// Data wire is plugged into port 2 on the Arduino
+#define ONE_WIRE_BUS 13
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+// arrays to hold device address
+DeviceAddress insideThermometer;
+
+void setup() {
+  Serial.begin(115200);
+
+  // locate devices on the bus
+  Serial.print("Locating devices...");
+  sensors.begin();
+  Serial.print("Found ");
+  Serial.print(sensors.getDeviceCount(), DEC);
+  Serial.println(" devices.");
+
+  // report parasite power requirements
+  Serial.print("Parasite power is: "); 
+  if (sensors.isParasitePowerMode()) Serial.println("ON");
+  else Serial.println("OFF");
+
+  if (!sensors.getAddress(insideThermometer, 0)) Serial.println("Unable to find address for Device 0"); 
+
+  // show the addresses we found on the bus
+  Serial.print("Device 0 Address: ");
+  printAddress(insideThermometer);
+  Serial.println();
+
+  // set the resolution to 9 bit (Each Dallas/Maxim device is capable of several different resolutions)
+  sensors.setResolution(insideThermometer, 9);
+ 
+  Serial.print("Device 0 Resolution: ");
+  Serial.println(sensors.getResolution(insideThermometer), DEC); 
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  //Send an HTTP POST request every 10 minutes
+  if ((millis() - lastTime) > timerDelay) {
+    //Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED){
+
+      sensors.requestTemperatures();
+
+      float tempC = sensors.getTempC(insideThermometer);
+  
+      HTTPClient http;
+      
+      http.begin(serverName);
+      http.addHeader("Content-Type", "application/json");
+
+      StaticJsonDocument<50> doc;
+      doc["Temperature"] = DallasTemperature::toFahrenheit(tempC);
+      
+      String requestBody;
+      serializeJson(doc, requestBody);
+       
+      int httpResponseCode = http.POST(requestBody);
+     
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+        
+      // Free resources
+      http.end();
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
+}
+
+// function to print a device address
+void printAddress(DeviceAddress deviceAddress)
+{
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    if (deviceAddress[i] < 16) Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+  }
+}
