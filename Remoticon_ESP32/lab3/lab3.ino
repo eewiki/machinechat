d73f7db8e7a5499805f771f0e3a8a45912fff7a8
Robert Nelson <robertcnelson@gmail.com>
2020-11-06 19:41:23 -0600

    these look messed up with tio

--- Remoticon_ESP32/lab3/lab3.ino
+++ Remoticon_ESP32/lab3/lab3.ino
@@ -33,7 +33,7 @@ void setup() {
   Serial.println();
 
   for (uint8_t t = 4; t > 0; t--) {
-    Serial.printf("[SETUP] WAIT %d...\n", t);
+    Serial.printf("[SETUP] WAIT %d...\r\n", t);
     Serial.flush();
     delay(500);
   }
